fbac0e355c66a780c2e8e2ede3b7975f27c32915
exosyphon <exosyphon@yahoo.com>
2014-08-03 16:33:49 -0600

    Initial commit

---
+++ android/src/com/courter/pandasurvivor/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.courter.pandasurvivor.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.courter.pandasurvivor.PandaSurvivor;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new PandaSurvivor(), config);
+	}
+}
