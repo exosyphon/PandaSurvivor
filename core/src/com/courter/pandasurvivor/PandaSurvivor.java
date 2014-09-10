a7025fb1e75c169934f49a30113783b9baa5df2f
exosyphon <exosyphon@yahoo.com>
2014-09-09 19:18:47 -0600

    Move most of the rendering into WorldRenderer

--- core/src/com/courter/pandasurvivor/PandaSurvivor.java
+++ core/src/com/courter/pandasurvivor/PandaSurvivor.java
@@ -2,79 +2,33 @@ package com.courter.pandasurvivor;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Sprite;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.maps.tiled.TiledMap;
-import com.badlogic.gdx.maps.tiled.TmxMapLoader;
-import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector3;
 
 public class PandaSurvivor extends ApplicationAdapter {
-    private static final int FRAME_COLS = 12;
-    private static final int FRAME_ROWS = 8;
     public static final int RIGHT_SIDE_OF_MAP = 7584;
     public static final int LEFT_SIDE_OF_MAP = 0;
     public static final int BOTTOM_OF_MAP = 0;
     public static final int TOP_OF_MAP = 7616;
     public static final float BUTTON_ACTION_BUFFER = 20;
     float lastActionTime = 0;
-    Rectangle aButtonBounds = new Rectangle(1590, 50, 196, 196);
-    OrthographicCamera camera;
-    OrthogonalTiledMapRendererWithSprites tiledMapRenderer;
-    Sprite heroSprite;
-    Sprite dpadSprite;
-    Sprite aButtonSprite;
-    TextureRegion[] firstPandaFrames;
-    Animation pandaDownAnimation;
-    Animation pandaLeftAnimation;
-    Animation pandaRightAnimation;
-    Animation pandaUpAnimation;
     float deltaTime;
-    TiledMap tiledMap;
     World world;
+    WorldRenderer worldRenderer;
 
 
     @Override
     public void create() {
         Assets.load();
-
-        float w = Gdx.graphics.getWidth();
-        float h = Gdx.graphics.getHeight();
-
-        camera = new OrthographicCamera();
-        camera.setToOrtho(false, w, h);
-
-        camera.update();
-
-        addHero(w, h);
-
-        setupControlSprites(w);
-
-        tiledMap = new TmxMapLoader().load(World.PANDA_SNOW_MAP_NAME);
-        tiledMapRenderer = new OrthogonalTiledMapRendererWithSprites(tiledMap);
-        tiledMapRenderer.addSprite(heroSprite);
-        tiledMapRenderer.addControlSprite(dpadSprite);
-        tiledMapRenderer.addControlSprite(aButtonSprite);
-
-        world = new World(null, tiledMapRenderer);
+        worldRenderer = new WorldRenderer();
+        world = new World(null, WorldRenderer.tiledMapRenderer);
     }
 
     @Override
     public void render() {
-        Gdx.gl.glClearColor(1, 0, 0, 1);
-        Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        camera.update();
-        tiledMapRenderer.setView(camera);
-        tiledMapRenderer.render();
-
         deltaTime = Gdx.graphics.getDeltaTime();
 
         world.update(deltaTime);
-        renderObjectSprites();
+        worldRenderer.render();
 
         if (Gdx.input.isTouched(0)) {
             touchDown(Gdx.input.getX(0), Gdx.input.getY(0));
@@ -88,14 +42,14 @@ public class PandaSurvivor extends ApplicationAdapter {
 
     public boolean touchDown(float screenX, float screenY) {
         Vector3 clickCoordinates = new Vector3(screenX, screenY, 0);
-        Vector3 clickPosition = camera.unproject(clickCoordinates);
+        Vector3 clickPosition = WorldRenderer.camera.unproject(clickCoordinates);
 
-        float heroOriginalX = heroSprite.getX();
-        float heroOriginalY = heroSprite.getY();
+        float heroOriginalX = WorldRenderer.heroSprite.getX();
+        float heroOriginalY = WorldRenderer.heroSprite.getY();
 
-        if (OverlapTester.pointInRectangle(aButtonBounds, clickPosition.x, clickPosition.y)) {
+        if (OverlapTester.pointInRectangle(WorldRenderer.aButtonBounds, clickPosition.x, clickPosition.y)) {
             handleAButtonPress(heroOriginalX, heroOriginalY);
-        } else if (clickPosition.x < (dpadSprite.getX() + dpadSprite.getWidth() + 80) && clickPosition.y < (dpadSprite.getY() + dpadSprite.getHeight() + 40)) {
+        } else if (clickPosition.x < (WorldRenderer.dpadSprite.getX() + WorldRenderer.dpadSprite.getWidth() + 80) && clickPosition.y < (WorldRenderer.dpadSprite.getY() + WorldRenderer.dpadSprite.getHeight() + 40)) {
             handleDpadMovement(clickPosition, heroOriginalX, heroOriginalY);
         }
         return true;
@@ -103,159 +57,38 @@ public class PandaSurvivor extends ApplicationAdapter {
 
     private void handleAButtonPress(float heroOriginalX, float heroOriginalY) {
         if (lastActionTime == 0 || lastActionTime > (deltaTime * BUTTON_ACTION_BUFFER)) {
-            updatePandaShootingSpriteTexture(World.hero.getCurrentDirection());
-
-            Sprite fireballSprite = new Sprite(Assets.fireball);
-            fireballSprite.setSize(32, 32);
-            fireballSprite.setPosition(heroOriginalX + (heroSprite.getWidth() / 2), heroOriginalY);
-            tiledMapRenderer.addSprite(fireballSprite);
-
-            World.fireballList.add(new Fireball(heroOriginalX + (heroSprite.getWidth() / 2), heroOriginalY, fireballSprite));
+            worldRenderer.updatePandaShootingSpriteTexture(World.hero.getCurrentDirection());
+            worldRenderer.addFireballSprite(heroOriginalX, heroOriginalY);
 
             lastActionTime = deltaTime;
         }
     }
 
     private void handleDpadMovement(Vector3 position, float heroOriginalX, float heroOriginalY) {
-        if (position.y < (dpadSprite.getY() + 180) && position.y > (dpadSprite.getY() + 68)) {
-            if (position.x < (dpadSprite.getX() + 125) && heroOriginalX > LEFT_SIDE_OF_MAP) {
+        if (position.y < (WorldRenderer.dpadSprite.getY() + 180) && position.y > (WorldRenderer.dpadSprite.getY() + 68)) {
+            if (position.x < (WorldRenderer.dpadSprite.getX() + 125) && heroOriginalX > LEFT_SIDE_OF_MAP) {
                 //left
                 World.hero.setCurrentDirection(World.HeroDirections.LEFT);
-                updatePandaWalkingSpriteTexture(World.HeroDirections.LEFT);
-                updateCameraAndPandaSpritePositionsLeft(heroOriginalX, heroOriginalY);
-            } else if (position.x > (dpadSprite.getX() + 148) && heroOriginalX < RIGHT_SIDE_OF_MAP) {
+                worldRenderer.updatePandaWalkingSpriteTexture(World.HeroDirections.LEFT);
+                worldRenderer.updateCameraAndPandaSpritePositionsLeft(deltaTime, heroOriginalX, heroOriginalY);
+            } else if (position.x > (WorldRenderer.dpadSprite.getX() + 148) && heroOriginalX < RIGHT_SIDE_OF_MAP) {
                 //right
                 World.hero.setCurrentDirection(World.HeroDirections.RIGHT);
-                updatePandaWalkingSpriteTexture(World.HeroDirections.RIGHT);
-                updateCameraAndPandaSpritePositionsRight(heroOriginalX, heroOriginalY);
+                worldRenderer.updatePandaWalkingSpriteTexture(World.HeroDirections.RIGHT);
+                worldRenderer.updateCameraAndPandaSpritePositionsRight(deltaTime, heroOriginalX, heroOriginalY);
             }
         } else {
-            if (position.y < (dpadSprite.getY() + 80) && heroOriginalY > BOTTOM_OF_MAP) {
+            if (position.y < (WorldRenderer.dpadSprite.getY() + 80) && heroOriginalY > BOTTOM_OF_MAP) {
                 //down
                 World.hero.setCurrentDirection(World.HeroDirections.DOWN);
-                updatePandaWalkingSpriteTexture(World.HeroDirections.DOWN);
-                updateCameraAndPandaSpritePositionsDown(heroOriginalX, heroOriginalY);
-            } else if (position.y > (dpadSprite.getY() + 148) && heroOriginalY < TOP_OF_MAP) {
+                worldRenderer.updatePandaWalkingSpriteTexture(World.HeroDirections.DOWN);
+                worldRenderer.updateCameraAndPandaSpritePositionsDown(deltaTime, heroOriginalX, heroOriginalY);
+            } else if (position.y > (WorldRenderer.dpadSprite.getY() + 148) && heroOriginalY < TOP_OF_MAP) {
                 //up
                 World.hero.setCurrentDirection(World.HeroDirections.UP);
-                updatePandaWalkingSpriteTexture(World.HeroDirections.UP);
-                updateCameraAndPandaSpritePositionsUp(heroOriginalX, heroOriginalY);
+                worldRenderer.updatePandaWalkingSpriteTexture(World.HeroDirections.UP);
+                worldRenderer.updateCameraAndPandaSpritePositionsUp(deltaTime, heroOriginalX, heroOriginalY);
             }
-        }
-    }
-
-    private void slurpPandaFramesIntoAnimations() {
-        Texture pandaSheet = Assets.pandaSpriteSheet;
-        TextureRegion[][] tmp = TextureRegion.split(pandaSheet, pandaSheet.getWidth() / FRAME_COLS, pandaSheet.getHeight() / FRAME_ROWS);              // #10
-        firstPandaFrames = new TextureRegion[(FRAME_COLS * FRAME_ROWS) / 2];
-        int index = 0;
-        //First 4 rows are blank in the sprite sheet
-        //First 3 columns are for the first panda
-        for (int i = 4; i < FRAME_ROWS; i++) {
-            for (int j = 0; j < 3; j++) {
-                firstPandaFrames[index++] = tmp[i][j];
-            }
-        }
-
-        pandaDownAnimation = new Animation(.2f, firstPandaFrames[0], firstPandaFrames[1], firstPandaFrames[2]);
-        pandaLeftAnimation = new Animation(.2f, firstPandaFrames[3], firstPandaFrames[4], firstPandaFrames[5]);
-        pandaRightAnimation = new Animation(.2f, firstPandaFrames[6], firstPandaFrames[7], firstPandaFrames[8]);
-        pandaUpAnimation = new Animation(.2f, firstPandaFrames[9], firstPandaFrames[10], firstPandaFrames[11]);
-    }
-
-    private void addHero(float w, float h) {
-        slurpPandaFramesIntoAnimations();
-
-        heroSprite = new Sprite(firstPandaFrames[0]);
-        heroSprite.setSize(96, 96);
-        heroSprite.setPosition(w / 2, h / 2);
-        World.hero = new Hero(w / 2, h / 2);
-    }
-
-    private void setupControlSprites(float w) {
-        dpadSprite = new Sprite(Assets.dpad);
-        dpadSprite.setPosition(75, 75);
-
-        aButtonSprite = new Sprite(Assets.aButton);
-        aButtonSprite.setPosition(w - Assets.aButton.getWidth() - 75, 100);
-    }
-
-    private void updateCameraAndPandaSpritePositionsLeft(float originalx, float originaly) {
-        heroSprite.setPosition(originalx - (World.HERO_MOVE_SPEED * deltaTime), originaly);
-        dpadSprite.setPosition(dpadSprite.getX() - (World.HERO_MOVE_SPEED * deltaTime), dpadSprite.getY());
-        aButtonBounds.setPosition(aButtonBounds.getX() - (World.HERO_MOVE_SPEED * deltaTime), aButtonBounds.getY());
-        aButtonSprite.setPosition(aButtonSprite.getX() - (World.HERO_MOVE_SPEED * deltaTime), aButtonSprite.getY());
-        camera.translate(-(World.HERO_MOVE_SPEED * deltaTime), 0);
-    }
-
-    private void updateCameraAndPandaSpritePositionsRight(float originalx, float originaly) {
-        heroSprite.setPosition(originalx + (World.HERO_MOVE_SPEED * deltaTime), originaly);
-        dpadSprite.setPosition(dpadSprite.getX() + (World.HERO_MOVE_SPEED * deltaTime), dpadSprite.getY());
-        aButtonBounds.setPosition(aButtonBounds.getX() + (World.HERO_MOVE_SPEED * deltaTime), aButtonBounds.getY());
-        aButtonSprite.setPosition(aButtonSprite.getX() + (World.HERO_MOVE_SPEED * deltaTime), aButtonSprite.getY());
-        camera.translate((World.HERO_MOVE_SPEED * deltaTime), 0);
-    }
-
-    private void updateCameraAndPandaSpritePositionsDown(float originalx, float originaly) {
-        heroSprite.setPosition(originalx, originaly - (World.HERO_MOVE_SPEED * deltaTime));
-        dpadSprite.setPosition(dpadSprite.getX(), dpadSprite.getY() - (World.HERO_MOVE_SPEED * deltaTime));
-        aButtonBounds.setPosition(aButtonBounds.getX(), aButtonBounds.getY() - (World.HERO_MOVE_SPEED * deltaTime));
-        aButtonSprite.setPosition(aButtonSprite.getX(), aButtonSprite.getY() - (World.HERO_MOVE_SPEED * deltaTime));
-        camera.translate(0, -(World.HERO_MOVE_SPEED * deltaTime));
-    }
-
-    private void updateCameraAndPandaSpritePositionsUp(float originalx, float originaly) {
-        heroSprite.setPosition(originalx, originaly + (World.HERO_MOVE_SPEED * deltaTime));
-        dpadSprite.setPosition(dpadSprite.getX(), dpadSprite.getY() + (World.HERO_MOVE_SPEED * deltaTime));
-        aButtonBounds.setPosition(aButtonBounds.getX(), aButtonBounds.getY() + (World.HERO_MOVE_SPEED * deltaTime));
-        aButtonSprite.setPosition(aButtonSprite.getX(), aButtonSprite.getY() + (World.HERO_MOVE_SPEED * deltaTime));
-        camera.translate(0, (World.HERO_MOVE_SPEED * deltaTime));
-    }
-
-    private void updatePandaShootingSpriteTexture(World.HeroDirections direction) {
-        switch (direction) {
-            case UP:
-                heroSprite.setRegion(pandaUpAnimation.getKeyFrame(World.hero.stateTime, Animation.ANIMATION_LOOPING));
-                break;
-            case DOWN:
-                heroSprite.setRegion(pandaDownAnimation.getKeyFrame(World.hero.stateTime, Animation.ANIMATION_LOOPING));
-                break;
-            case LEFT:
-                heroSprite.setRegion(pandaLeftAnimation.getKeyFrame(World.hero.stateTime, Animation.ANIMATION_LOOPING));
-                break;
-            case RIGHT:
-                heroSprite.setRegion(pandaRightAnimation.getKeyFrame(World.hero.stateTime, Animation.ANIMATION_LOOPING));
-                break;
-        }
-
-    }
-
-    private void updatePandaWalkingSpriteTexture(World.HeroDirections direction) {
-        switch (direction) {
-            case UP:
-                heroSprite.setRegion(pandaUpAnimation.getKeyFrame(World.hero.stateTime, Animation.ANIMATION_LOOPING));
-                break;
-            case DOWN:
-                heroSprite.setRegion(pandaDownAnimation.getKeyFrame(World.hero.stateTime, Animation.ANIMATION_LOOPING));
-                break;
-            case LEFT:
-                heroSprite.setRegion(pandaLeftAnimation.getKeyFrame(World.hero.stateTime, Animation.ANIMATION_LOOPING));
-                break;
-            case RIGHT:
-                heroSprite.setRegion(pandaRightAnimation.getKeyFrame(World.hero.stateTime, Animation.ANIMATION_LOOPING));
-                break;
-        }
-    }
-
-    private void renderObjectSprites() {
-        renderFireballs();
-    }
-
-    private void renderFireballs() {
-        int len = World.fireballList.size();
-        for (int i = 0; i < len; i++) {
-            Fireball fireball = World.fireballList.get(i);
-            fireball.getSprite().setPosition(fireball.position.x, fireball.position.y);
         }
     }
 }
--- core/src/com/courter/pandasurvivor/WorldRenderer.java
+++ core/src/com/courter/pandasurvivor/WorldRenderer.java
@@ -1,7 +1,183 @@
 package com.courter.pandasurvivor;
 
-/**
- * Created by andrew on 9/9/14.
- */
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.math.Rectangle;
+
 public class WorldRenderer {
+    public static final Rectangle aButtonBounds = new Rectangle(1590, 50, 196, 196);
+    private static final int FRAME_ROWS = 8;
+    private static final int FRAME_COLS = 12;
+    public static Sprite aButtonSprite;
+    public static Sprite dpadSprite;
+    public static Sprite heroSprite;
+    public static OrthogonalTiledMapRendererWithSprites tiledMapRenderer;
+    public static OrthographicCamera camera;
+    TextureRegion[] firstPandaFrames;
+    Animation pandaDownAnimation;
+    Animation pandaLeftAnimation;
+    Animation pandaRightAnimation;
+    Animation pandaUpAnimation;
+    TiledMap tiledMap;
+
+
+    public WorldRenderer() {
+        float w = Gdx.graphics.getWidth();
+        float h = Gdx.graphics.getHeight();
+
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, w, h);
+
+        camera.update();
+
+        addHero(w, h);
+
+        setupControlSprites(w);
+
+        tiledMap = new TmxMapLoader().load(World.PANDA_SNOW_MAP_NAME);
+        tiledMapRenderer = new OrthogonalTiledMapRendererWithSprites(tiledMap);
+        tiledMapRenderer.addSprite(heroSprite);
+        tiledMapRenderer.addControlSprite(dpadSprite);
+        tiledMapRenderer.addControlSprite(aButtonSprite);
+    }
+    
+    public void render() {
+        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        camera.update();
+        tiledMapRenderer.setView(camera);
+        tiledMapRenderer.render();
+
+        renderObjectSprites();
+    }
+
+    public void renderObjectSprites() {
+        renderFireballs();
+    }
+
+    public void addHero(float w, float h) {
+        slurpPandaFramesIntoAnimations();
+
+        heroSprite = new Sprite(firstPandaFrames[0]);
+        heroSprite.setSize(96, 96);
+        heroSprite.setPosition(w / 2, h / 2);
+        World.hero = new Hero(w / 2, h / 2);
+    }
+    
+    public void addFireballSprite(float heroOriginalX, float heroOriginalY) {
+        Sprite fireballSprite = new Sprite(Assets.fireball);
+        fireballSprite.setSize(32, 32);
+        fireballSprite.setPosition(heroOriginalX + (heroSprite.getWidth() / 2), heroOriginalY);
+        tiledMapRenderer.addSprite(fireballSprite);
+        World.fireballList.add(new Fireball(heroOriginalX + (WorldRenderer.heroSprite.getWidth() / 2), heroOriginalY, fireballSprite));
+    }
+
+    public void updatePandaShootingSpriteTexture(World.HeroDirections direction) {
+        switch (direction) {
+            case UP:
+                WorldRenderer.heroSprite.setRegion(pandaUpAnimation.getKeyFrame(World.hero.stateTime, Animation.ANIMATION_LOOPING));
+                break;
+            case DOWN:
+                WorldRenderer.heroSprite.setRegion(pandaDownAnimation.getKeyFrame(World.hero.stateTime, Animation.ANIMATION_LOOPING));
+                break;
+            case LEFT:
+                WorldRenderer.heroSprite.setRegion(pandaLeftAnimation.getKeyFrame(World.hero.stateTime, Animation.ANIMATION_LOOPING));
+                break;
+            case RIGHT:
+                WorldRenderer.heroSprite.setRegion(pandaRightAnimation.getKeyFrame(World.hero.stateTime, Animation.ANIMATION_LOOPING));
+                break;
+        }
+
+    }
+
+    public void updatePandaWalkingSpriteTexture(World.HeroDirections direction) {
+        switch (direction) {
+            case UP:
+                WorldRenderer.heroSprite.setRegion(pandaUpAnimation.getKeyFrame(World.hero.stateTime, Animation.ANIMATION_LOOPING));
+                break;
+            case DOWN:
+                WorldRenderer.heroSprite.setRegion(pandaDownAnimation.getKeyFrame(World.hero.stateTime, Animation.ANIMATION_LOOPING));
+                break;
+            case LEFT:
+                WorldRenderer.heroSprite.setRegion(pandaLeftAnimation.getKeyFrame(World.hero.stateTime, Animation.ANIMATION_LOOPING));
+                break;
+            case RIGHT:
+                WorldRenderer.heroSprite.setRegion(pandaRightAnimation.getKeyFrame(World.hero.stateTime, Animation.ANIMATION_LOOPING));
+                break;
+        }
+    }
+
+    public void updateCameraAndPandaSpritePositionsLeft(float deltaTime, float originalx, float originaly) {
+        WorldRenderer.heroSprite.setPosition(originalx - (World.HERO_MOVE_SPEED * deltaTime), originaly);
+        WorldRenderer.dpadSprite.setPosition(WorldRenderer.dpadSprite.getX() - (World.HERO_MOVE_SPEED * deltaTime), WorldRenderer.dpadSprite.getY());
+        aButtonBounds.setPosition(aButtonBounds.getX() - (World.HERO_MOVE_SPEED * deltaTime), aButtonBounds.getY());
+        WorldRenderer.aButtonSprite.setPosition(WorldRenderer.aButtonSprite.getX() - (World.HERO_MOVE_SPEED * deltaTime), WorldRenderer.aButtonSprite.getY());
+        WorldRenderer.camera.translate(-(World.HERO_MOVE_SPEED * deltaTime), 0);
+    }
+
+    public void updateCameraAndPandaSpritePositionsRight(float deltaTime, float originalx, float originaly) {
+        WorldRenderer.heroSprite.setPosition(originalx + (World.HERO_MOVE_SPEED * deltaTime), originaly);
+        WorldRenderer.dpadSprite.setPosition(WorldRenderer.dpadSprite.getX() + (World.HERO_MOVE_SPEED * deltaTime), WorldRenderer.dpadSprite.getY());
+        aButtonBounds.setPosition(aButtonBounds.getX() + (World.HERO_MOVE_SPEED * deltaTime), aButtonBounds.getY());
+        WorldRenderer.aButtonSprite.setPosition(WorldRenderer.aButtonSprite.getX() + (World.HERO_MOVE_SPEED * deltaTime), WorldRenderer.aButtonSprite.getY());
+        WorldRenderer.camera.translate((World.HERO_MOVE_SPEED * deltaTime), 0);
+    }
+
+    public void updateCameraAndPandaSpritePositionsDown(float deltaTime, float originalx, float originaly) {
+        WorldRenderer.heroSprite.setPosition(originalx, originaly - (World.HERO_MOVE_SPEED * deltaTime));
+        WorldRenderer.dpadSprite.setPosition(WorldRenderer.dpadSprite.getX(), WorldRenderer.dpadSprite.getY() - (World.HERO_MOVE_SPEED * deltaTime));
+        aButtonBounds.setPosition(aButtonBounds.getX(), aButtonBounds.getY() - (World.HERO_MOVE_SPEED * deltaTime));
+        WorldRenderer.aButtonSprite.setPosition(WorldRenderer.aButtonSprite.getX(), WorldRenderer.aButtonSprite.getY() - (World.HERO_MOVE_SPEED * deltaTime));
+        WorldRenderer.camera.translate(0, -(World.HERO_MOVE_SPEED * deltaTime));
+    }
+
+    public void updateCameraAndPandaSpritePositionsUp(float deltaTime, float originalx, float originaly) {
+        WorldRenderer.heroSprite.setPosition(originalx, originaly + (World.HERO_MOVE_SPEED * deltaTime));
+        WorldRenderer.dpadSprite.setPosition(WorldRenderer.dpadSprite.getX(), WorldRenderer.dpadSprite.getY() + (World.HERO_MOVE_SPEED * deltaTime));
+        aButtonBounds.setPosition(aButtonBounds.getX(), aButtonBounds.getY() + (World.HERO_MOVE_SPEED * deltaTime));
+        WorldRenderer.aButtonSprite.setPosition(WorldRenderer.aButtonSprite.getX(), WorldRenderer.aButtonSprite.getY() + (World.HERO_MOVE_SPEED * deltaTime));
+        WorldRenderer.camera.translate(0, (World.HERO_MOVE_SPEED * deltaTime));
+    }
+
+    private void renderFireballs() {
+        int len = World.fireballList.size();
+        for (int i = 0; i < len; i++) {
+            Fireball fireball = World.fireballList.get(i);
+            fireball.getSprite().setPosition(fireball.position.x, fireball.position.y);
+        }
+    }
+
+    private void slurpPandaFramesIntoAnimations() {
+        Texture pandaSheet = Assets.pandaSpriteSheet;
+        TextureRegion[][] tmp = TextureRegion.split(pandaSheet, pandaSheet.getWidth() / FRAME_COLS, pandaSheet.getHeight() / FRAME_ROWS);              // #10
+        firstPandaFrames = new TextureRegion[(FRAME_COLS * FRAME_ROWS) / 2];
+        int index = 0;
+        //First 4 rows are blank in the sprite sheet
+        //First 3 columns are for the first panda
+        for (int i = 4; i < FRAME_ROWS; i++) {
+            for (int j = 0; j < 3; j++) {
+                firstPandaFrames[index++] = tmp[i][j];
+            }
+        }
+
+        pandaDownAnimation = new Animation(.2f, firstPandaFrames[0], firstPandaFrames[1], firstPandaFrames[2]);
+        pandaLeftAnimation = new Animation(.2f, firstPandaFrames[3], firstPandaFrames[4], firstPandaFrames[5]);
+        pandaRightAnimation = new Animation(.2f, firstPandaFrames[6], firstPandaFrames[7], firstPandaFrames[8]);
+        pandaUpAnimation = new Animation(.2f, firstPandaFrames[9], firstPandaFrames[10], firstPandaFrames[11]);
+    }
+
+    private void setupControlSprites(float w) {
+        dpadSprite = new Sprite(Assets.dpad);
+        dpadSprite.setPosition(75, 75);
+
+        aButtonSprite = new Sprite(Assets.aButton);
+        aButtonSprite.setPosition(w - Assets.aButton.getWidth() - 75, 100);
+    }
 }
